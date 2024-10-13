b5bcba595fa791bd1c7584d074f0c9a8bc0a4506
Colin Leung <colin@cowise.co>
2024-10-13 11:35:44 +0800

    feat: v2 filename and configs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import url from "node:url";
-const dirname = url.fileURLToPath(new URL('.', import.meta.url)).replace(/\/$/, '');
-export default {dirname}
-
+export default {
+  filename: import.meta.url,
+}
 import FormCaptchaAdapterRecaptcha from "./classes/adapter/Recaptcha.mjs";
 
 export {
